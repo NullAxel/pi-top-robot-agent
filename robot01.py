1fc2ea55b484e2c2bfaa865b400aa02b827acc1f
naielv <109038805+naielv@users.noreply.github.com>
2023-04-30 18:43:41 +0200

    Create robot01.py

---
+++ robot01.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template
+from pitop import Pitop
+from pitop.robotics.drive_controller import DriveController
+from time import sleep
+from pitop import Buzzer
+from pitop import LED
+import pyttsx3
+import os
+robot = Pitop()
+# Note: The ports in here are CUSTOM, so please do not make a issue because of this.
+drive = DriveController(left_motor_port="M3", right_motor_port="M2")
+robot.add_component(drive)
+buzzer = Buzzer("D7")
+led1 = LED("D6")
+led2 = LED("D5")
+app = Flask(__name__)
+
+@app.route('/l1') ## LIGHTS ON
+def l1():
+    led1.on()
+    led2.on()
+    return str(robot.battery.capacity)
+
+@app.route('/l0') ## LIGHTS OFF
+def l0():
+    led1.off()
+    led2.off()
+    return str(robot.battery.capacity)
+
+@app.route('/w') # DRIVE W (FORWARDS)
+def w():
+    robot.drive.forward(0.25)
+    sleep(1)
+    robot.drive.stop()
+    return str(robot.battery.capacity)
+
+@app.route('/s') ## DRIVE S (BACKWARDS)
+def s():
+    robot.drive.forward(-0.25)
+    sleep(1)
+    robot.drive.stop()
+    return str(robot.battery.capacity)
+
+@app.route('/a') ## DRIVE A (LEFT)
+def a():
+    robot.drive.left(0.25)
+    sleep(1)
+    robot.drive.stop()
+    return str(robot.battery.capacity)
+
+@app.route('/d') ## DRIVE D (RIGHT)
+def d():
+    robot.drive.right(0.25)
+    sleep(1)
+    robot.drive.stop()
+    return str(robot.battery.capacity)
+@app.route('/q') ## STOP DRIVING
+def q():
+    robot.drive.stop()
+    sleep(1)
+    robot.drive.stop()
+    return str(robot.battery.capacity)
+@app.route("/b") ## BUZZ HALF A SEC
+def b():
+    buzzer.on()
+    sleep(.5)
+    buzzer.off()
+    return str(robot.battery.capacity)
+@app.route("/t/<msg>") ## TEXT TO SPEECH
+def t(msg):
+    os.popen(f"gtts-cli '{msg}' -l es | play -t mp3 -")
+    return str(robot.battery.capacity)
+
+if __name__ == '__main__':
+    robot.miniscreen.display_multiline_text("Axel", font_size=50)
+    app.run(host='0.0.0.0', port=9999)
